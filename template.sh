d5d4ae425c72e292275f5f9bda81d468effabd7a
Tanvi Yerawar <yerawartanvi79@gmail.com>
2026-03-20 16:36:21 +0530

    folder structure added

---
+++ template.sh
@@ -0,0 +1,11 @@
+mkdir -p src
+mkdir -p research
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.ipyb
+touch requirements.txt
+echo "Directory and files created successfully"
